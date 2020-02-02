4d1a5438418cb6ea24323a06ac87710b7e70adb9
Mostafizor Rahman <mostafizor1997@gmail.com>
2020-02-02 23:10:10 +0000

    Update block.py

--- blockchain/block.py
+++ blockchain/block.py
@@ -1,5 +1,9 @@
 class Block(object):
-    def __init__(self, blockHeaders):
-        self.blockHeaders = blockHeaders
-    
+    def __init__(self, blockHeader):
+        self.blockHeader = blockHeader
+
+    @staticmethod
+    def genesis(GENESIS_DATA):
+        return Block(GENESIS_DATA)
+
     
